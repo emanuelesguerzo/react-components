194319f9c89c20c1388d168dd9615f7914b30608
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-12 15:41:11 +0100

    feat: footer complete and code cleaning

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,9 @@
+import style from './AppFooter.module.css';
+
 const AppFooter = () => {
     return (
-        <footer className="container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam facilis officiis beatae sint dolorem voluptatum quos nesciunt doloribus enim assumenda reiciendis, voluptate aperiam autem perferendis debitis similique, ipsam culpa a!</p>
+        <footer className={style.footer}>
+            <p className={style.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam facilis officiis beatae sint dolorem voluptatum quos nesciunt doloribus enim assumenda reiciendis, voluptate aperiam autem perferendis debitis similique, ipsam culpa a!</p>
         </footer>
     )
 }
